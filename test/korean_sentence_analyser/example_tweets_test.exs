73411c4f383155baa806796a2e59b7ceb14e026a
JorisKok <joris_kok1@hotmail.com>
2019-12-22 09:30:30 +0100

    Add more example sentences in tests

--- test/korean_sentence_analyser/example_tweets_test.exs
+++ test/korean_sentence_analyser/example_tweets_test.exs
@@ -37,5 +37,61 @@ defmodule ExampleTweetsTest do
         ]
       )
     end
+
+    test "@user ㄱ..그냥ㅋㅋㅋ" do
+      assert_value KoreanSentenceAnalyser.analyse_sentence("@user ㄱ..그냥ㅋㅋㅋ") == [
+                     %{"specific_type" => "Noun", "token" => "그냥", "type" => "Noun"}
+                   ]
+    end
+
+    test "카세료보다 당신이 더 대단하다고!!!!" do
+      assert_value KoreanSentenceAnalyser.analyse_sentence("카세료보다 당신이 더 대단하다고!!!!") == [
+                     %{"specific_type" => "Wikipedia title noun", "token" => "카세료", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "당신", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "더", "type" => "Noun"},
+                     %{"specific_type" => "Adjective", "token" => "대단하다", "type" => "Adjective"}
+                   ]
+    end
+
+    test "Long string ㅋㅋㅋㅋㅋ" do
+      assert_value KoreanSentenceAnalyser.analyse_sentence(
+                     "@user ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ오른ㄴ쪽손엨ㅋㅋㅋㅋ컄ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅁㅋㅋㅋㅋㅋㅋㅇㅋㅇㅋㅌㅌㅋㅋㅋ그거먼ㄴ데옄ㅋㅋㅋㅋㅋㅋㅋ"
+                   ) == [
+                     %{"specific_type" => "Noun", "token" => "오른", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "쪽", "type" => "Noun"},
+                     %{"specific_type" => "Family name", "token" => "손", "type" => "Substantive"},
+                     %{"specific_type" => "Noun", "token" => "그거", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "데", "type" => "Noun"}
+                   ]
+    end
+
+    test "@user 젠장 나는 이나라에 살아서 받는게 도대체 무엇이야" do
+      assert_value KoreanSentenceAnalyser.analyse_sentence("@user 젠장 나는 이나라에 살아서 받는게 도대체 무엇이야") == [
+                     %{"specific_type" => "Noun", "token" => "젠장", "type" => "Noun"},
+                     %{"specific_type" => "Verb", "token" => "나다", "type" => "Verb"},
+                     %{"specific_type" => "Wikipedia title noun", "token" => "이나라", "type" => "Noun"},
+                     %{"specific_type" => "Adjective", "token" => "살다", "type" => "Adjective"},
+                     %{"specific_type" => "Verb", "token" => "받다", "type" => "Verb"},
+                     %{"specific_type" => "Noun", "token" => "도대체", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "무엇", "type" => "Noun"}
+                   ]
+    end
+
+    test "RT @user: [SS현장] '고열량-無표시' 한정판 햄버거… '알 면 안 먹습니다' http://link.com" do
+      assert_value KoreanSentenceAnalyser.analyse_sentence(
+                     "RT @user: [SS현장] '고열량-無표시' 한정판 햄버거… '알 면 안 먹습니다' http://link.com"
+                   ) == [
+                     %{"specific_type" => "Noun", "token" => "현장", "type" => "Noun"},
+                     # TODO 고열량
+                     %{"specific_type" => "Noun", "token" => "열량", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "표시", "type" => "Noun"},
+                     %{"specific_type" => "Wikipedia title noun", "token" => "한정판", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "햄버거", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "알", "type" => "Noun"},
+                     %{"specific_type" => "Noun", "token" => "면", "type" => "Noun"},
+                     %{"specific_type" => "Family name", "token" => "안", "type" => "Substantive"},
+                     %{"specific_type" => "Verb", "token" => "먹다", "type" => "Verb"}
+                   ]
+    end
   end
 end
