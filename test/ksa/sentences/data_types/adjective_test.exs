82f30cfe6e32ca78c615cd57a1df52435bd06669
JorisKok <joris_kok1@hotmail.com>
2020-04-19 20:57:48 +0200

    Update adjective_test.exs and noun_test.exs

---
+++ test/ksa/sentences/data_types/adjective_test.exs
@@ -0,0 +1,20 @@
+defmodule Sentences.DataTypes.AdjectiveTest do
+  use ExUnit.Case
+  import Ksa, only: [analyse: 1]
+  alias Ksa.Structs.Adjective
+  
+  test "옛날의 서울의 모습이 몹시 그립습니다" do
+    result = analyse("옛날의 서울의 모습이 몹시 그립습니다")
+    
+    assert Enum.member?(
+             result,
+             %Adjective{
+               conjugated: nil,
+               match: "그립",
+               type: "adjective",
+               word: "그립습니다"
+             }
+           )
+  
+  end
+end
--- test/ksa/sentences/data_types/noun_test.exs
+++ test/ksa/sentences/data_types/noun_test.exs
@@ -1,10 +1,10 @@
 defmodule Sentences.DataTypes.NounTest do
   use ExUnit.Case
-  import Ksa.DataTypes.Noun, only: [match: 1]
+  import Ksa, only: [analyse: 1]
   alias Ksa.Structs.Noun
 
   test "내 고향은 서울입니다" do
-    result = match("내 고향은 서울입니다")
+    result = analyse("내 고향은 서울입니다")
 
     assert Enum.member?(result, %Noun{match: "내", word: "내", type: "noun", subtype: "noun"})
     assert Enum.member?(result, %Noun{match: "고향", word: "고향은", type: "noun", subtype: "noun"})
@@ -12,7 +12,7 @@ defmodule Sentences.DataTypes.NounTest do
   end
 
   test "서울시장에 갑시다" do
-    result = match("서울시장에 갑시다")
+    result = analyse("서울시장에 갑시다")
 
     assert Enum.member?(result, %Noun{match: "서울", word: "서울시장에", type: "noun", subtype: "noun"})
     assert Enum.member?(result, %Noun{match: "시장", word: "서울시장에", type: "noun", subtype: "noun"})
