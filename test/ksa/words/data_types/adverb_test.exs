499c823ba93166aeff76c40a9a521eab5e2e0862
JorisKok <joris_kok1@hotmail.com>
2020-04-22 08:57:17 +0200

    Add adverbs

---
+++ test/ksa/words/data_types/adverb_test.exs
@@ -0,0 +1,17 @@
+defmodule Words.DataTypes.VerbTest do
+  use ExUnit.Case
+  import Ksa.DataTypes.Verb, only: [match: 1]
+  alias Ksa.Structs.Verb
+  
+  test "만지지마" do
+    assert Enum.member?(
+             match("만지지마"),
+             %Verb{
+               conjugated: nil,
+               match: "만지",
+               type: "verb",
+               word: "만지지마"
+             }
+           )
+  end
+end
