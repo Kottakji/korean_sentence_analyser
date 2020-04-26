cd69a4e74d33c2cbc830367899383df8028c2090
JorisKok <joris_kok1@hotmail.com>
2020-04-26 11:06:50 +0200

    Update .travis.yml, korean_sentence_analyser.ex, and 51 more files...

--- mix.exs
+++ mix.exs
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-defmodule KoreanSentenceAnalyser.MixProject do
-=======
 defmodule Ksa.MixProject do
->>>>>>> develop
   use Mix.Project
 
   def project do
     [
-<<<<<<< HEAD
-      app: :korean_sentence_analyser,
-      version: "0.3.1",
-=======
       app: :ksa,
       version: "0.4.0",
->>>>>>> develop
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -26,28 +17,6 @@ defmodule Ksa.MixProject do
       name: "Korean Sentence Analyser",
       source_url: "https://github.com/JorisKok/korean_sentence_analyser",
       docs: [
-<<<<<<< HEAD
-        main: "KoreanSentenceAnalyser",
-        groups_for_modules: [
-          Main: [KoreanSentenceAnalyser],
-          DataTypes: [
-            KSA.Adjective,
-            KSA.Adverb,
-            KSA.Conjunction,
-            KSA.Determiner,
-            KSA.Eomi,
-            KSA.Josa,
-            KSA.ModifiedNoun,
-            KSA.Modifier,
-            KSA.Noun,
-            KSA.PreEomi,
-            KSA.Substantive,
-            KSA.Verb,
-            KSA.Grammar
-          ],
-          ETS: [KSA.DictFile],
-          Helpers: [KSA.Formatter, KSA.KoreanUnicode, KSA.LocalDict, KSA.SplitWord, KSA.Stem, KSA.Typo, KSA.Word]
-=======
         main: "Ksa",
         groups_for_modules: [
           Main: [Ksa],
@@ -63,7 +32,6 @@ defmodule Ksa.MixProject do
             Ksa.Structs.Substantive,
             Ksa.Structs.Verb
           ]
->>>>>>> develop
         ],
         extras: ["README.md"]
       ]
@@ -86,11 +54,7 @@ defmodule Ksa.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-<<<<<<< HEAD
-      mod: {KoreanSentenceAnalyser, []},
-=======
       mod: {Ksa, []},
->>>>>>> develop
       extra_applications: [:logger]
     ]
   end
--- test/korean_sentence_analyser_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule KoreanSentenceAnalyserTest do
-  use ExUnit.Case
-  doctest KoreanSentenceAnalyser
-end
